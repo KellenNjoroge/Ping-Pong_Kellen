15c2df61b1a9f5b5d4653336db2238ad354ef98a
KellenNjoroge <muthonkel@gmail.com>
2018-07-13 14:36:12 +0300

    addedd initial html structure

---
+++ js/scrpts.js
@@ -0,0 +1,18 @@
+for (var i=1,i <= 20 i++)
+{
+    if (% 15 == 0)
+
+       console.log("pingPong");
+
+    else if (i % 3 == )
+
+        console.log ("ping");
+
+    else if (i % 5 == 0)
+
+         console.log("pong");
+
+    else 
+        console.log(i);
+
+}
